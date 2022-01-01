c7455d5bc6c3ce61a64b9a14ccd8763f2ac5ec41
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2022-01-01 15:38:44 +0330

    change document title according to page title with intersection observer api

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-document.title = 'test';
+// Change document title to current h1 textContent
+const changeDocumentTitle = function (enteries) {
+  if (enteries.length > 1 && enteries[1].isIntersecting) {
+    document.title = enteries[1].target.textContent;
+  } else if (enteries[0].isIntersecting) {
+    document.title = enteries[0].target.textContent;
+  } else {
+    return;
+  }
+
+  console.log(enteries);
+};
+
+// Get all h1 elements in document
+let headings = document.getElementsByTagName('h1');
+
+// Initialize Intersection Observer
+const observer = new IntersectionObserver(changeDocumentTitle);
+
+// Check Each H1 element in document
+[...headings].forEach((element) => {
+  observer.observe(element);
+});
